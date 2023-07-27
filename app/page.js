3622626a6ef9dcf63fbae851c7016f39bef3f49a
MarinJursic <marin.jursic@gmail.com>
2023-07-27 18:38:20 +0200

    inital commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,48 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import styles from "@/styles/page.module.scss";
+import { InputAdornment, MenuItem, OutlinedInput, Select } from "@mui/material";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [crypto, setCrypto] = useState("");
+  const [cryptoAmount, setCryptoAmount] = useState(undefined);
+
+  const handleChange = (prop) => (e) => {
+    if (prop === "crypto") {
+      setCrypto(e.target.value);
+    } else if (prop === "amount") {
+      setCryptoAmount(e.target.value);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <section className={styles.exchanger}>
+        <div className={styles.crypto}>
+          <h2>Select Crypto</h2>
+          <Select
+            labelId="crypto-select-label"
+            id="crypto-select"
+            value={crypto}
+            onChange={handleChange("crypto")}
+            className={styles.sel}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <MenuItem value={"BTC"}>BTC</MenuItem>
+            <MenuItem value={"BCH"}>BCH</MenuItem>
+            <MenuItem value={"LTC"}>LTC</MenuItem>
+          </Select>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            value={cryptoAmount === undefined ? "" : cryptoAmount}
+            onChange={handleChange("amount")}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
+            placeholder="0.00"
+          />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
 }
