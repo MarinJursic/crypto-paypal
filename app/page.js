4a5317e8eac6c991ed02d7509591fa2db9741aed
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2023-07-30 23:38:05 +0200

    Added a switch a function which iterates between crypto to paypal and paypal to crypto functions. Also, added paypal to crypto function.

--- app/page.js
+++ app/page.js
@@ -7,41 +7,103 @@ import { useState } from "react";
 
 export default function Home() {
   const [crypto, setCrypto] = useState("");
+  const [email, setEmail] = useState("");
+  const [wallet, setWallet] = useState("");
   const [cryptoAmount, setCryptoAmount] = useState(undefined);
+  const [mode, setMode] = useState("crypto");
 
   const handleChange = (prop) => (e) => {
-    if (prop === "crypto") {
-      setCrypto(e.target.value);
-    } else if (prop === "amount") {
-      setCryptoAmount(e.target.value);
+    switch (prop) {
+      case "crypto":
+        setCrypto(e.target.value);
+        break;
+      case "amount":
+        setCryptoAmount(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "wallet":
+        setWallet(e.target.value);
+        break;
+      default:
+        break;
     }
   };
 
   return (
     <main className={styles.main}>
       <section className={styles.exchanger}>
-        <div className={styles.crypto}>
-          <h2>Select Crypto</h2>
-          <Select
-            labelId="crypto-select-label"
-            id="crypto-select"
-            value={crypto}
-            onChange={handleChange("crypto")}
-            className={styles.sel}
-          >
-            <MenuItem value={"BTC"}>BTC</MenuItem>
-            <MenuItem value={"BCH"}>BCH</MenuItem>
-            <MenuItem value={"LTC"}>LTC</MenuItem>
-          </Select>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            value={cryptoAmount === undefined ? "" : cryptoAmount}
-            onChange={handleChange("amount")}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Amount"
-            placeholder="0.00"
-          />
-        </div>
+        {mode === "crypto" ? (
+          <div className={styles.crypto}>
+            <h2>Crypto to Paypal</h2>
+            <Select
+              labelId="crypto-select-label"
+              id="crypto-select"
+              value={crypto}
+              onChange={handleChange("crypto")}
+              className={styles.sel}
+            >
+              <MenuItem value={"BTC"}>BTC</MenuItem>
+              <MenuItem value={"BCH"}>BCH</MenuItem>
+              <MenuItem value={"LTC"}>LTC</MenuItem>
+            </Select>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={cryptoAmount === undefined ? "" : cryptoAmount}
+              onChange={handleChange("amount")}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+              label="Amount"
+              placeholder="0.00"
+            />
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              onChange={handleChange("email")}
+              placeholder="Paypal mail"
+            />
+            <button
+              onClick={() =>
+                setMode((prev) => (prev === "crypto" ? "paypal" : "crypto"))
+              }
+            >
+              Switch
+            </button>
+          </div>
+        ) : (
+          <div className={styles.crypto}>
+            <h2>Paypal to Crypto</h2>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={cryptoAmount === undefined ? "" : cryptoAmount}
+              onChange={handleChange("amount")}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+              label="Amount"
+              placeholder="0.00"
+            />
+            <Select
+              labelId="crypto-select-label"
+              id="crypto-select"
+              value={crypto}
+              onChange={handleChange("crypto")}
+              className={styles.sel}
+            >
+              <MenuItem value={"BTC"}>BTC</MenuItem>
+              <MenuItem value={"BCH"}>BCH</MenuItem>
+              <MenuItem value={"LTC"}>LTC</MenuItem>
+            </Select>
+
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              onChange={handleChange("wallet")}
+              placeholder="Wallet Address"
+            />
+            <button>Switch</button>
+          </div>
+        )}
       </section>
     </main>
   );
